c83c4eb00f5bde799999a9e6e670247810d007cf
John Smith <example@stanford.edu>
2013-07-10 21:09:46 +0000

    Modified index.html

--- web.js
+++ web.js
@@ -3,7 +3,7 @@ var express = require('express');
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  var s=buf.toString(fs.readFileSync("index.html"));
+  var s=fs.readFileSync("index.html").toString;
  response.send(s);
 });
 
